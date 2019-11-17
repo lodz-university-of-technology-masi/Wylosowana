d7665137c1e30350aa0414acb75964e5944b9880
bomblozaurus <49494671+bomblozaurus@users.noreply.github.com>
2019-11-17 18:51:40 +0100

    created test is sent to api and saved in db now

--- src/components/recruiter/CreateTest.js
+++ src/components/recruiter/CreateTest.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
+import $ from 'jquery';
 
 class CreateTest extends Component {
     constructor(props) {
@@ -10,7 +11,11 @@ class CreateTest extends Component {
             name: '',
             lang: '',
             questions: [],
+            tempQuestion: '',
         }
+
+        this.handleQuestionTextChange = this.handleQuestionTextChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleNameChange = (event) => {
@@ -25,6 +30,12 @@ class CreateTest extends Component {
         })
     };
 
+    handleQuestionTextChange(e) {
+        this.setState({
+            tempQuestion: e.target.value
+        })
+    };
+
     handleOpenQuestionQuestionChange = idx => evt => {
         const newQuestions = this.state.questions.map((que, qidx) => {
             if (idx !== qidx) return que;
@@ -35,7 +46,7 @@ class CreateTest extends Component {
 
     handleAddOpenQuestion = () => {
         this.setState({
-            questions: this.state.questions.concat([{no: 0, guestion: ""}])
+            questions: [...this.state.questions, this.state.tempQuestion]
         });
     };
 
@@ -46,10 +57,44 @@ class CreateTest extends Component {
     };
 <<<<<<< HEAD
 
+<<<<<<< HEAD
     createJson(){
+=======
+    handleSubmit(event) {
+        console.log(this.state.name);
+        const questions = this.state.questions
+            .filter(v => v != '')
+            .map( (val,ind) => {
+                return { "no" : ind, "question" : val}
+            })
+        console.log(questions);
 
+        const validateTest = {
+            "testName": this.state.name,
+            "langs": [
+                {
+                    "lang": this.state.lang,
+                    "questions": questions
+                }
+            ],
+            "candidate_ids": []
+        }
+>>>>>>> created test is sent to api and saved in db now
+
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: 'https://jqt7k6tt7i.execute-api.us-east-1.amazonaws.com/demo/tests',
+            data: JSON.stringify(validateTest),
+            success: function(data,err){
+                if(err)
+                    console.log(err);
+                console.log(data);
+            }
+        });
     }
 
+<<<<<<< HEAD
     // eslint-disable-next-line no-undef
 /*    @action
     async add(data) {
@@ -80,8 +125,10 @@ class CreateTest extends Component {
     };
 =======
 >>>>>>> Unnecessary files deleted and changed some view
+=======
 
-    /*
+>>>>>>> created test is sent to api and saved in db now
+
         handleAddCloseQuestion = () => {
             this.setState({
                 questions: this.state.questions.concat([{no: 0, guestion: "",answers:[],correct:[]}])
@@ -99,6 +146,7 @@ class CreateTest extends Component {
     render() {
         return (
             <section className="section auth">
+<<<<<<< HEAD
             <Form>
                 <h1>Test</h1>
                 <Form.Group controlId="controlInputName">
@@ -122,6 +170,37 @@ class CreateTest extends Component {
                 ))}
                 <Button id="addOpenQuestionButton" variant="info" onClick={this.handleAddOpenQuestion}>Add Open Question</Button>
             </Form>
+=======
+                <Form onSubmit={this.handleSubmit}>
+                    <h1>Test</h1>
+                    <Form.Group controlId="controlInputName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="name" placeholder="Name" onChange={this.handleNameChange}/>
+                    </Form.Group>
+                    <Form.Group controlId="controlSelectLang">
+                        <Form.Label>Select a language</Form.Label>
+                        <Form.Control as="select" onChange={this.handleLanguageChange.bind(this)} value={this.state.language}>
+                            <option value="EN">English</option>
+                            <option value="PL">Polish</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <h4>Questions</h4>
+                    {this.state.questions.map((que, idx) => (
+                        <Form.Group controlId="controlQuestion">
+                            <Form.Label>Question</Form.Label>
+                            <Form.Control as="textarea" rows="3" onChange={this.handleQuestionTextChange} />
+                            <Button id="deleteButton" onClick={this.handleRemoveOpenQuestion(idx)} variant="danger"
+                                    size="sm">Delete</Button>
+                        </Form.Group>
+                    ))}
+                    <Button id="addOpenQuestionButton"  variant="info" onClick={this.handleAddOpenQuestion} >Add Open
+                        Question</Button>
+                    <div>
+                     <Button id="addOpenQuestionButton" type="submit" variant="info"
+                             className="mt-3" onClick={this.handleAddOpenQuestion}> Save Test </Button>
+                    </div>
+                </Form>
+>>>>>>> created test is sent to api and saved in db now
             </section>
         )
     }
