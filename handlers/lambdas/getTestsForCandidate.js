8151f0c875e00b93b4e3f8b7693486964a592a7e
bomblozaurus <49494671+bomblozaurus@users.noreply.github.com>
2019-11-29 03:40:58 +0100

    add lambda for getting all tests assigned to particular candidate, change model for posting new test

---
+++ handlers/lambdas/getTestsForCandidate.js
@@ -0,0 +1,40 @@
+'use strict'
+const AWS = require('aws-sdk');
+
+exports.handler = async(event, context) => {
+  const documentClient = new AWS.DynamoDB.DocumentClient();
+
+  let responseBody = "";
+  let statusCode = 0;
+
+  const candidate_login = event.pathParameters.login;
+
+  const params = {
+    TableName: "Tests",
+    ProjectionExpression: "id, testName, langs",
+    FilterExpression: "contains (candidate_logins, :candidate_login)",
+    ExpressionAttributeValues: {
+      ":candidate_login": candidate_login
+    }
+  };
+
+  try {
+    const data = await documentClient.scan(params).promise();
+    responseBody = JSON.stringify(data);
+    statusCode = 200;
+
+  } catch (err) {
+    responseBody = `Unable to get tests for candidate [${event.pathParameters}]: ${err}`;
+    statusCode = 403;
+  }
+
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: responseBody
+  };
+
+  return response;
+};
