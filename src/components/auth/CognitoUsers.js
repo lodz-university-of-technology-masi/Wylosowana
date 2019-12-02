bdf46598ef3e37e1cf9ad35bbf53af77cc94c77d
goloneczka <48791689+goloneczka@users.noreply.github.com>
2019-12-02 12:33:49 +0100

    delete credits

--- src/components/auth/CognitoUsers.js
+++ src/components/auth/CognitoUsers.js
@@ -12,7 +12,6 @@ export const params = {
 export const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
     apiVersion: '2016-04-18',
     region: config.cognito.REGION,
-    credentials: new AWS.Credentials('ASIAZ2J3YGNP6GIVNXNN', 'kw2BLE0MqywZ2LTlkeRtrFnp0knuB/lPXiWGk+xq',
-        'FwoGZXIvYXdzEMn//////////wEaDJABDLtrWpp8294uJSLDAdNeDGc8HHL9LzXrflIyyAJhcAizl1Emm9qocbH1RJt+0fpbOek+3u2uwX+SxpnJwexgdKvttASzOqTRiRIMtwNTGoaAU0WlBVufsX9XotPS/NWWY6BnqziWAG+hqHsB2Tifs9TaQzjcBlGaMaEvM3p8CUXAJ/VykQZAaeUbnMVCtnw2hVQ36EHmOE6LzZU+oxu6kiF9APz5jCf4cthEe48Fwspinqzd7BhBm7C9yf9HlBWDzOtq3hjlNPPa6UMnV1yuXSiqs4/vBTItr/KPpdyD1usNVeX7eqfRWWGaMUS2gTy+MCIFP6CJL73wT73IdOac0Zs9Dc/3') // !!! what here
+    credentials: new AWS.Credentials() // !!! uzupelnic, klucze od Seby !!!
 });
 
