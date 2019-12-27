37b4bf52c73291b457e52e39be942e83f2ab03af
Paula Zachacz <paula.zachacz@onet.pl>
2019-12-27 19:01:15 +0100

    Empty key update

--- src/components/auth/CognitoUsers.js
+++ src/components/auth/CognitoUsers.js
@@ -11,5 +11,5 @@ export const params = {
 export const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
     apiVersion: '2016-04-18',
     region: config.cognito.REGION,
-    credentials: new AWS.Credentials('ASIAZ2J3YGNPZTUU46PT', '/QqGoOcbeyRvER9ENPTg+M7bjOFuFtdqHWJ5WFNx', 'FwoGZXIvYXdzEDoaDOmNv2uDQD+ozX5oGyLDARfXzhevBCgCIE5JANED9LpSFnJ8YOPZR5jBr7vpa4Tpvt/6rQ2mOj18F0pVhXWeRLJUZYvV8+w+akUc+VuzfImZBbETVlKUTAcMMNVLimPVMjthF0rjMqluHJk+CZ2gfAB2bixvw8lZmTtLw1YVhjKY4MKUBqn+Fvf9uyHqJHxjZl0MhSCu9O4TKkCqddbeTcotIZL++IzHi49jVZEo+ZeeNDymb4Ez/7UGZc34YREpWE9na6IR/dRg8mRxiIBspZEnByj53pjwBTItJ/2C6k66WLih1L99J53rmlsOKkEu19z+AOQKmWBVqyDPDONSwUerJVJjXHLs') /// !!! klucze od Seby
+    credentials: new AWS.Credentials('') /// !!! klucze od Seby
 });
