8d4619704e3bc17ac6adc985e882bb2e76949ed8
rjehf <45949354+rjehf@users.noreply.github.com>
2020-01-18 14:39:05 +0100

    delete old code

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,5 +1,13 @@
 const Constants = {
     TRANSLATE: require('yandex-translate')('trnsl.1.1.20191219T153918Z.bad67cf36c9d3490.89ddc43eea27a08206cf10db04a374b748ad5d16'),
     DICTIONARY: require('yandex-dictionary')('dict.1.1.20200108T131311Z.8bda1d7d97c3a36a.4c363b15079be40c15229e09711c35a6d1d30d15')
-}
+};
 export default Constants
+
+export const URL = {
+    cognito: {
+        listCandidates: 'https://dkue6ysvr4.execute-api.us-east-1.amazonaws.com/dev/candidates',
+        deleteUser: 'https://dkue6ysvr4.execute-api.us-east-1.amazonaws.com/dev/deleteuser/',
+        createUser: 'https://dkue6ysvr4.execute-api.us-east-1.amazonaws.com/dev/createUser',
+    }
+};
--- src/components/auth/CognitoUsers.js
+++ src/components/auth/CognitoUsers.js
@@ -1,24 +1,10 @@
-import config from "../../config";
 import axios from "axios";
 import {Auth} from "aws-amplify";
-
-const  AWS = require('aws-sdk');
-
-export const params = {
-    UserPoolId: config.cognito.USER_POOL_ID,
-    AttributesToGet: ["name"], // if null return all
-    Filter: 'name ^= \"Candidate\"',
-};
-
-export const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
-    apiVersion: '2016-04-18',
-    region: config.cognito.REGION,
-    credentials: new AWS.Credentials('ASIAZ2J3YGNP4TSV4W53','gVX9wrR03DG2RrGv5lZsGMKlAVAIK1NdA58+SjP3', 'FwoGZXIvYXdzEEMaDEtX9sWCjXGMtZbwMiLDAaUnCtRl5ThNGpVz1gXdbfLvV61GxYx737Vgf5oalQjqjGjFg8WkaOG7MzvDdMMPcFhC5iwgPFOCvMbe1tpo4r9dOe75DSwtYvb/EfsrhMn6EM174CRpCUVyrhGHIkd53uPH3rYdX3zwK6SWBN32XIWyUIx+VL2PgGA4btndAwjNoB+ja5FfEfib66UBvITYp2XxiNpNH07IwlK3CD5ITumt073+uIApHjCFpmLKZpGI3qLRdLOtZr1MqEDGiKb0Gw2WmiiIlIvxBTItRx34xDVWpznBULcWtEeYafMlOP05KGrzmQRJh2lJzrWjkXAsYG/JbzJCpXO7') /// !!! klucze od Seby
-});
+import {URL} from "../Constants"
 
 export async function listCandidates() {
    return  axios
-        .get('https://dkue6ysvr4.execute-api.us-east-1.amazonaws.com/dev/candidates', {
+        .get(URL.cognito.listCandidates, {
             headers: {
                 'Content-Type': 'application/json',
                 'authorization': `${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
@@ -28,20 +14,20 @@ export async function listCandidates() {
 
 export async function deleteUser(username) {
     return  axios
-        .delete('https://dkue6ysvr4.execute-api.us-east-1.amazonaws.com/dev/deleteuser/' + username, {
+        .delete(URL.cognito.deleteUser + username, {
             headers: {
                 'Content-Type': 'application/json',
-                'authorization': `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
+                'authorization': `${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
             },
         });
 }
 
 export async function createUser(username, forceAliasCreation, temporaryPassword, email, profile) {
     return  axios
-        .post('https://dkue6ysvr4.execute-api.us-east-1.amazonaws.com/dev/createUser', {
+        .post(URL.cognito.createUser, {
             headers: {
                 'Content-Type': 'application/json',
-                'authorization': `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
+                'authorization': `${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
             },
                 "username": username,
                 "forceAliasCreation": forceAliasCreation,
@@ -49,7 +35,6 @@ export async function createUser(username, forceAliasCreation, temporaryPassword
                 "email": email,
                 "profile": profile,
                 "name": profile,
-
         });
 }
 
