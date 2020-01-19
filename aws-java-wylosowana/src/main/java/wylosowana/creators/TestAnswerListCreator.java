34ffe9251a7053b1bcc9bccfac1a2e403c9994d4
bomblozaurus <49494671+bomblozaurus@users.noreply.github.com>
2020-01-19 22:39:48 +0100

    remove old project

--- aws-java-wylosowana/src/main/java/wylosowana/creators/TestAnswerListCreator.java
+++
@@ -1,4 +0,0 @@
-package wylosowana.creators;
-
-public class TestAnswerListCreator {
-}
--- aws-java-wylosowana/src/main/java/wylosowana/validator/ParticipantValidator.java
+++
@@ -1,9 +0,0 @@
-package wylosowana.validator;
-
-import wylosowana.model.Participant;
-
-public class ParticipantValidator {
-    public static boolean checkNull(Participant participant) {
-        return participant != null;
-    }
-}
